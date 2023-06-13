ed885644123b13efcf6a89e3eb944e488ae3ea58
Zar Ni Khin Kyi <77061456+zarnikhinkyi@users.noreply.github.com>
2023-06-14 01:47:02 +0800

    Add files via upload

---
+++ data_cleaning.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE projects;
+
+USE projects;
+
+SELECT * FROM hr;
+
+/*change column name
+ALTER TABLE hr
+CHANGE COLUMN ï»¿id emp_id VARCHAR(20) NULL;
+
+DESCRIBE hr;
+
+SELECT birthdate FROM hr;
+
+SET sql_safe_updates = 0;
+
+UPDATE hr
+SET birthdate = CASE
+	WHEN birthdate LIKE '%/%' THEN date_format(str_to_date(birthdate, '%m/%d/%Y'), '%Y-%m-%d')
+    WHEN birthdate LIKE '%-%' THEN date_format(str_to_date(birthdate, '%m-%d-%Y'), '%Y-%m-%d')
+    ELSE NULL
+END;
+
+
+ALTER TABLE hr
+MODIFY COLUMN birthdate DATE;
+
+UPDATE hr
+SET hire_date = CASE
+	WHEN hire_date LIKE '%/%' THEN date_format(str_to_date(hire_date, '%m/%d/%Y'), '%Y-%m-%d')
+    WHEN hire_date LIKE '%-%' THEN date_format(str_to_date(hire_date, '%m-%d-%Y'), '%Y-%m-%d')
+    ELSE NULL
+END;
+
+ALTER TABLE hr
+MODIFY COLUMN hire_date DATE;
+
+UPDATE hr
+SET termdate = date(str_to_date(termdate, '%Y-%m-%d %H:%i:%s UTC'))
+WHERE termdate IS NOT NULL AND termdate != ' ';
+
+ALTER TABLE hr
+MODIFY COLUMN termdate DATE;
+
+SELECT * FROM hr;
+SELECT termdate FROM hr;
+
+ALTER TABLE hr
+ADD COLUMN age INT;
+
+UPDATE hr
+SET age = timestampdiff(YEAR,birthdate,CURDATE());
+
+SELECT birthdate, age FROM hr;
+
+SELECT min(age) AS YOUNG,
+max(age) AS OLDEST FROM hr;
+
+SELECT COUNT(*) FROM hr
+WHERE age<18;
